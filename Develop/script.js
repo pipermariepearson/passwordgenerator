0d9ac66cf65b0c3f11bcd0db5147a2f7f3e19182
Piper Pearson <pipermariepearson@gmail.com>
2020-02-25 21:57:33 -0800

    better design based on what i learned this class

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,77 @@
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+{
+function pwLength(){
+
+  var howMany = prompt("how many characters do you need");
+
+  if (howMany > 8 && howMany < 128) {
+  confirm("Your pass word will be " + howMany + " characters long.");
+  console.log(howMany);
+  } else {
+    alert("Your password is either too long or too short. Please try again!");
+  }
+}
+}
+
+{
+  function specialChara(){
+    
+    var special = confirm("do you need special characters?");
+
+    if (special === true) {
+      alert("Your password will have special characters in it." );
+      console.log(special);
+    } else {
+      alert("Your password will NOT have special characters in it.");
+      console.log(special);
+    }
+    }
+  }
+
+
+
+ 
+
+
+
+//Call functions
+
+ pwLength();
+
+ specialChara();
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Write password to the #password input
+
+
+
+
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,23 +80,16 @@ function writePassword() {
 
 }
 
-function passCriteria (){
-  
-  var length =  prompt("What is the length of your new password?");
-   if (length > 8 && length < 128) {
-     prompt("lowercase, uppercase, numeric, and/or special characters");
-   } else {
-     alert("password is too short or too long");
-   }
 
 
-}
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-generateBtn.addEventListener("click",passCriteria);
 
-passCriteria;
+
 
 
